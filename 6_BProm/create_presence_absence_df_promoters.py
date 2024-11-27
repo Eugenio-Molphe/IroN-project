9ba2479a6d9b23af25d59bfb2b79899055a0e33f
Eugenio-Molphe <eugenio.perez@mls.uzh.ch>
2024-11-27 12:01:37 +0100

    Made the supplementary tables

--- 6_BProm/create_presence_absence_df_promoters.py
+++ 6_BProm/create_presence_absence_df_promoters.py
@@ -14,10 +14,11 @@ import os
 # Let's get the arguments
 inputDir = sys.argv[1]
 outputDf = sys.argv[2]
+numberBpUpstream = int(sys.argv[3])
 
 # Functions, baby
 
-def get_best_promoters(file, promotersDict):
+def get_best_promoters(file, promotersDict, bpUsptream):
     """
     This function reads the file and returns a new item for the dictionary: 
     genomeName:promoters for iroN in that genome
@@ -30,6 +31,9 @@ def get_best_promoters(file, promotersDict):
         # Let's get the genome name
         fileLines = f.readlines()
 
+    # Let' obtain the length of the sequence and then the maximum distance upstream in which were found the promoters
+    seqLength = int(fileLines[1].split(" ")[-1].strip())
+
     # This file contains an identified position and the promotor that may be there
     # So, let's get a list with the promoters
 
@@ -67,35 +71,27 @@ def get_best_promoters(file, promotersDict):
 
     for promoter in promoters:
         # I'll create a dictionary to store the information: keys as scores and values as the name of the promoter
-        promoterDict = {}
         for oligo in promoter:
             oligo = oligo.strip().split(" ")
             namePromoter = oligo[0].strip(':') # We've got the name of the promoter
-    #### This lines are in case that you want to access the promoters with the highest scores
-    #        if oligo[-1] != 'sites':
-    #            score = int(oligo[-1])
-    #            promoterDict[score] = namePromoter
-
-
-        # Now, let's get the promoter with the highest score
-    #    if promoterDict:
-    #        maxScore = max(promoterDict.keys())
-    #        bestPromoter = promoterDict[maxScore]
-    #        iroNpromoters.append(bestPromoter)
     
-    #### This line is in case that you want to access the first promoters
+    #### Let's get the promoters that are closer to the iroN gene
             if oligo[-1] != 'sites':
                 try:
                     distance = int(oligo[10])
                 except:
-                    distance = int(oligo[11])
-                promoterDict[distance] = namePromoter
-        
-        # Now, let's get the promoter with the highest score
-        if promoterDict:
-            minDistance = min(promoterDict.keys())
-            bestPromoter = promoterDict[minDistance]
-            iroNpromoters.append(bestPromoter)
+                    try:
+                        distance = int(oligo[11])
+                    except:
+                        try:
+                            distance = int(oligo[9])
+                        except:
+                            distance = int(oligo[8])
+
+    # Now, I'm going to include all the promoters that are inside the range
+                distance = seqLength - distance
+                if distance <= bpUsptream:
+                    iroNpromoters.append(namePromoter)
 
     # Finally, let's save the result in the dictionary
     if iroNpromoters:
@@ -103,85 +99,21 @@ def get_best_promoters(file, promotersDict):
         
     return promotersDict
 
-def get_promoters(file, promotersDict):
-    """
-    This function reads the file and returns a new item for the dictionary: 
-    genomeName:promoters for iroN in that genome
-    This one doesn't filter the promoters, it gets all of them  
-    """
 
-    # First, let's open the file
-
-    with open(file, 'r') as f:
-        # Let's get the genome name
-        fileLines = f.readlines()
-
-    # This file contains an identified position and the promotor that may be there
-    # So, let's get a list with the promoters
-
-    # First, I'll filter the lines before the first promoter
-    startProcessing = False
-    filteredLines = []
-    for line in fileLines:
-        if line.startswith("For promoter"):
-            startProcessing = True
-        if startProcessing:
-            filteredLines.append(line)
-
-    # Then, I'll get the promoters
-    promoters = []
-    currentPromoter = []
-    # Iterate over the lines and dump the possible promoters into the currentPromoter list
-    for line in filteredLines:
-        if line.startswith("For promoter"):
-            promoters.append(currentPromoter)
-            currentPromoter = []
-            continue
-        # Skip adding the "For promoter" line to the current section, we only care about the identified promoters
-        currentPromoter.append(line)
-
-    # Don't forget to add the last section if it exists
-    if currentPromoter:
-        promoters.append(currentPromoter)
-    
-    # The first one is the name of the genome, we don't need it in the list per se
-    genomeName = fileLines[0].split('.')[1].strip('>')
-
-    # Now, let's get the list of the most possible promoters per each identified promoters
-    # In other words, the promoters influencing iroN
-    iroNpromoters = []
-
-    for promoter in promoters:
-        # I'll create a dictionary to store the information: keys as scores and values as the name of the promoter
-        for oligo in promoter:
-            oligo = oligo.strip().split(" ")
-            namePromoter = oligo[0].strip(':') # We've got the name of the promoter
-            if oligo[-1] != 'sites':
-                iroNpromoters.append(namePromoter)
-    # Finally, let's save the result in the dictionary
-    if iroNpromoters:
-        promotersDict[genomeName] = iroNpromoters
-        
-    return promotersDict
 
 
 
 ### Main ###
 
 # Let's create a dictionary that will store the promoters for each genome
-allPromotersDict = {}
-bestPromotersDict = {}
+promotersDict = {}
 
 # Let's iterate over the files in the directory and save the promoters in the dictionary
 fileList = os.listdir(inputDir)
 
-# This line gets all the promoters available in the files
-for file in fileList:
-    get_promoters(inputDir + '/' + file, allPromotersDict)
-
 # This line gets only the promoters with the highest score
 for files in fileList:
-    get_best_promoters(inputDir + '/' + files, bestPromotersDict)
+    get_best_promoters(inputDir + '/' + files, promotersDict, numberBpUpstream)
 
 
 
@@ -190,27 +122,18 @@ for files in fileList:
 # Extract all the unique promoters from all the genomes to get the columns of the dataframe
 allPromoters = set()
 
-for promoterList in allPromotersDict.values():  # For a dataframe with all the promoters
+for promoterList in promotersDict.values():  # For a dataframe with all the promoters
     for promoter in promoterList:
         allPromoters.add(promoter)
 allPromoters = sorted(list(allPromoters))
 
-#for promoter in bestPromotersDict.values():  # For a dataframe with only the best promoters
-#    allPromoters.update(promoter)
-#allPromoters = sorted(list(allPromoters))
-
 # Create the dataframe with the genomes as rows and promoters as columns
-df = pd.DataFrame(0, index = allPromotersDict.keys(), columns = allPromoters)
+df = pd.DataFrame(0, index = promotersDict.keys(), columns = allPromoters)
 
 # Let's fill the dataframe, the same applies with allPromotersDict or bestPromotersDict
-for genome, promoters in allPromotersDict.items():
+for genome, promoters in promotersDict.items():
     for promoter in promoters:
         df.loc[genome, promoter] = 1
-
-# This extra line add one to the promoters that were judged to be the ones with the highest score
-for genome, promoters in bestPromotersDict.items():
-    for promoter in promoters:
-        df.loc[genome, promoter] += 1
 
 # Save the dataframe
 df.to_csv(outputDf, sep = '\t', index=True, header=True)
